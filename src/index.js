3fa168df91c7b4841f3b44d7f4f6d1e5e1a67c22
GottaLearnThis <31788650+GottaLearnThis@users.noreply.github.com>
2021-05-22 10:26:32 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> Lenders For Investors</h1>
+
+    <h2> Find Funding For Your Real Estate Deals</h2>
+  </div>,
+
+  document.getElementById("root")
+);
